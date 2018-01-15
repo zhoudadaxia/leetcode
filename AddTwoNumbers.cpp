f191e04dc4db09ebad016804c843cbf4913f2664
z <z_houye@126.com>
2018-01-15 15:11:49 +0800

    add two number

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -6,40 +6,28 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+static const auto _____ = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* result = NULL;
-        ListNode* pre = NULL;
-        int carry = 0;
-        while(l1 || l2){
-            int v1 = l1 ? l1->val : 0;
-            int v2 = l2 ? l2->val : 0;
-            
-            int cur = v1 + v2 + carry;
-            int unit = cur % 10;
-            carry = cur / 10;
-            
-            if(!result){
-                result = new ListNode(unit);
-                pre = result;
-            } else {
-                pre->next = new ListNode(unit);
-                pre = pre->next;
-            }
-            
-            l1 = l1 ? l1->next : NULL;
-            l2 = l2 ? l2->next : NULL;
-        }
+        ListNode *head = new ListNode(0);
+        ListNode *tail = head;
         
-        if(carry > 0){
-            if(!result){
-                result = new ListNode(carry);
-            } else {
-                pre->next = new ListNode(carry);
-            }
+        int carry = 0;
+        while(l1 || l2 || carry){
+            int n = (l1?l1->val:0) + (l2?l2->val:0) + carry;
+            tail -> next = new ListNode(n % 10);
+            carry = n / 10;
+            tail = tail -> next;
+            l1 = l1?l1 -> next:NULL;
+            l2 = l2?l2 -> next:NULL;
         }
-         
-        return result;
+        return head -> next;
     }
 };
